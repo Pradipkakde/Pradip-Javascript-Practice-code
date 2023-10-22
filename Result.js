1191b939972859565157d2127a907f796b11b478
Pradipkakde <kakde.pradip1@gmail.com>
2023-10-22 19:55:46 +0530

    done changes

---
+++ Result.js
@@ -0,0 +1,18 @@
+let marks=99;
+if(marks>90 && marks>=100)
+{
+console.log("Destination")
+
+}
+else if(marks>60 && marks<=80 )
+{
+    console.log("First class")
+}
+else if(marks>35 && marks<=60 )
+{
+    console.log("Pass")
+}
+else 
+{
+    console.log("fails")
+}
