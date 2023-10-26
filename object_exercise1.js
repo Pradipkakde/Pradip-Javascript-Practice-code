590a73eafc2081762503cc8d424604cdd79202f8
Pradipkakde <kakde.pradip1@gmail.com>
2023-10-26 16:47:33 +0530

    odd

---
+++ object_exercise1.js
@@ -0,0 +1,14 @@
+const car={
+    color:'red',
+    cc:800,
+    price:500000,
+    fuel:'petrol'
+}
+function pcar(name,color,cc,price,fuel){
+    return{
+        color:Red,
+        cc:800,
+        price:50000,
+        fuel:petrol
+    }
+}
